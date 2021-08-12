6971ff5c48f597681925be0dc387791c6f4f76cd
Leonardo Felix Scandura <Leo@MacBook-Pro-de-Leonardo.local>
2021-08-12 17:04:21 -0300

    first commit

---
+++ src/junitTestes/MainTesteConversores.java
@@ -0,0 +1,41 @@
+package junitTestes;
+
+import org.junit.Test;
+
+import imgFactory.imagens.Extensoes;
+import imgFactory.imagens.Imagem;
+import imgFactory.imagens.conversor.conversores.*;
+
+
+public class MainTesteConversores {
+	
+	@Test
+	public void testeConversorPNG() {
+	String helper = System.getProperty("user.dir");
+	
+	Imagem batman = new Imagem();
+	batman.setPath(helper + "/src/junitTestes/IMGTeste/batman.jpg");
+	batman.setExtensao(Extensoes.JPG);
+		Conversor.fazerConversao(batman, "batmanPNG", new PNG());
+	}
+	
+	@Test
+	public void testeConversorBMP() {
+	String helper = System.getProperty("user.dir");
+	
+	Imagem batman = new Imagem();
+	batman.setPath(helper + "/src/junitTestes/IMGTeste/batman.jpg");
+	batman.setExtensao(Extensoes.JPG);
+		Conversor.fazerConversao(batman, "batmanBMP", new BMP());
+	}
+	
+	@Test
+	public void testeConversorJPG() {
+	String helper = System.getProperty("user.dir");
+	
+	Imagem batman = new Imagem();
+	batman.setPath(helper + "/src/junitTestes/IMGTeste/cryptex.bmp");
+	batman.setExtensao(Extensoes.JPG);
+		Conversor.fazerConversao(batman, "cryptexJPGConvertido", new JPG());
+	}
+}
